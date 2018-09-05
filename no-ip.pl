0c105d799b92843726763eae845c06230723559c
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2018-09-05 23:05:37 +0200

    Manage records call.

--- no-ip.pl
+++ no-ip.pl
@@ -4,6 +4,7 @@ use v5.28;
 use feature 'signatures';
 use strict;
 use warnings;
+no warnings 'experimental';
 
 use Config::Tiny;
 use Data::Dumper;
@@ -12,16 +13,29 @@ use LWP::Protocol::https;
 use REST::Client;
 
 my $CONFIG = Config::Tiny->read('no-ip.cfg')->{_};
-$CONFIG->{domain} =~ /\.(.*\..+)$/;
-$CONFIG->{zone} = $1;
+$CONFIG->{domain} =~ /^(.*)\.(.*\..+)$/;
+$CONFIG->{zone} = $2;
+$CONFIG->{sub} = $1;
 my $API = REST::Client->new();
 
+my $record_ipv4 = get_record($CONFIG->{sub}, 'A');
+my $record_ipv6 = get_record($CONFIG->{sub}, 'AAAA');
 
+say Dumper $record_ipv4;
+say Dumper $record_ipv6;
+
+# All queries that starts with get will get a cached value of the wanted attribute.
 sub get_uuid () {
 	state $uuid = call_uuid();
 	return $uuid;
 }
 
+sub get_record ($name, $type) {
+	state %records;
+	return $records{"$name-$type"} //= call_record($name, $type);
+}
+
+# All queries that starts with call will do a real call to the web-service.
 sub call($query) {
 	$API->addHeader('X-Api-Key', $CONFIG->{key});
 	my $result =
@@ -40,4 +54,11 @@ sub call_uuid {
 	return $uuid;
 }
 
-say get_uuid();
+sub call_record($name, $type) {
+	my @records = call('zones/' . get_uuid() . '/records');
+	for my $record (@records) {
+		next unless $record->{'rrset_name'} eq $name;
+		next unless $record->{'rrset_type'} eq $type;
+		return $record;
+	}
+}
